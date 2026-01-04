789c424682af56281fca5253d3b724eeab57e4fa
Passw <Passw_Passw@163.com>
2026-01-04 16:00:25 +0800

    .

--- src/ninja.cc
+++ src/ninja.cc
@@ -259,7 +259,8 @@ int GuessParallelism() {
   case 2:
     return 3;
   default:
-    return processors + 2;
+    return 1;
+    // return processors + 2;
   }
 }
 
@@ -1673,7 +1674,7 @@ class DeferGuessParallelism {
   BuildConfig* config;
 
   DeferGuessParallelism(BuildConfig* config)
-      : needGuess(true), config(config) {}
+      : needGuess(false), config(config) {}
 
   void Refresh() {
     if (needGuess) {
